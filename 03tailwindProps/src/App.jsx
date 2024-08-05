f7467c6bcad148e53b88ab7178c24f84d1d9d156
Vividh <vividhcho8430@gmail.com>
2024-08-05 21:04:18 +0530

    Tailwind with React and used Props

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+     <Card username= "Radhika" btntext = "Click me" />
+    </>
+  )
+}
+
+export default App
