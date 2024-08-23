cd5ff996db0634e5654ba4d6e634e5a09777e05e
Vividh <vividhcho8430@gmail.com>
2024-08-23 18:34:32 +0530

    updated authentication

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -96,15 +96,39 @@ export class Service{
     }
 
 
-
     async uploadFile(file){
         try {
-            
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
         } catch (error) {
-            throw error
+            console.log("Appwrite serive :: uploadFile :: error", error);
             return false
         }
     }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+    
 
 }
 
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/Container";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
